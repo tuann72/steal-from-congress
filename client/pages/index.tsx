e01ce26846bcd8273434bbbe266afad858648f48
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-03-22 01:17:31 -0500

    Displays backend data to the frontend.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,23 +1,48 @@
 import React, {useEffect, useState} from 'react'
 
+interface transactionData {
+  amount: number;
+  asset_description: string;
+  cap_gains_over_200_usd: boolean;
+  disclosure_date: string;
+  disclosure_year: string;
+  district: string;
+  industry: string;
+  owner: string;
+  party: string;
+  ptr_link: string;
+  representative: string;
+  sector: string;
+  state: string;
+  ticker: string;
+  transaction_date: string;
+  type: string;
+}
+
 function index() {
 
-  const [message, setMessage] = useState("Loading")
-  const [people, setPeople] = useState([]);
+  const [data, setData] = useState<transactionData[]>([]);
   useEffect(() => {
-    fetch("http://localhost:8080/api/home")
+    fetch("http://localhost:8080/api")
     .then((response) => response.json())
-    .then((data) => {
-      setMessage(data.message);
-      setPeople(data.people);
+    .then((data : transactionData[]) => {
+      setData(data);
     });
   },[]);
   return(
     <div>
-      <div>{message}</div>
-      {people.map((person, index) => (
-        <div key={index}>{person}</div>
-      ))}
+      <h1>House Stock Watcher</h1>
+      <div>
+        {data.map((item, index) => (
+          <div key={index}>
+            <p>Representative: {item.representative}</p>
+            <p>Asset: {item.asset_description}</p>
+            <p>Transaction Date: {item.transaction_date}</p>
+            <p>Amount: {item.amount}</p>
+            <hr></hr>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
